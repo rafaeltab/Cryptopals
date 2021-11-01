0b8a75a4c22279b80b8ec7a91660785d108a50c9
Rafael <ikbenrafaelbieze2001@gmail.com>
2021-11-01 21:08:54 +0100

    Add Set2 Challenge 9

---
+++ Rafaeltab.Cryptopals/Rafaeltab.Cryptopals.Set2.Challenge9/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Rafaeltab.Cryptopals.Set2.Common;
+using System.Text;
+
+Console.WriteLine(Encoding.UTF8.GetString(Padding.PKCS7(Encoding.UTF8.GetBytes("YELLOW SUBMARINE"),20)));
---
+++ Rafaeltab.Cryptopals/Rafaeltab.Cryptopals.Set2.Common/Padding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rafaeltab.Cryptopals.Set2.Common
+{
+    public static class Padding
+    {
+        public static byte[] PKCS7(byte[] data, int blockSize) {
+            var padAmount = (int)(Math.Floor(blockSize/(double)data.Length)*blockSize - data.Length);
+            if (padAmount == 0) return data;
+
+            Array.Resize(ref data, data.Length + blockSize);
+
+            for (int i = 0; i < padAmount; i++)
+            {
+                data[data.Length + i - blockSize] = 0x04;
+            }
+
+            return data;
+        }
+    }
+}
